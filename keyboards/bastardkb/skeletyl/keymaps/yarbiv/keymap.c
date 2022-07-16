19118fa15e02e0900c21c6af2b6694b6a8b79094
Yoav Arbiv <yoav.arbiv@gmail.com>
2022-07-15 22:21:37 -0700

    one should be more meticulous when copying connors code

--- keyboards/bastardkb/skeletyl/keymaps/yarbiv/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/yarbiv/keymap.c
@@ -67,3 +67,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+	if (!process_os_print(keycode, record, QC_POS)) { return false; }
+
+	return true;
+}
